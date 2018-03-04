a0b93c1d1b437d6abe3e4d0ea4e48e2a72fa3eba
adrbin <35666777+adrbin@users.noreply.github.com>
2018-03-04 21:56:58 +0100

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Adrian on 2018/02/17.
+//  Copyright © 2018 Adrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var deck = PlayingCardDeck()
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        for _ in 1...10 {
+//            if let card = deck.draw() {
+//                print("\(card)")
+//            }
+//        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
